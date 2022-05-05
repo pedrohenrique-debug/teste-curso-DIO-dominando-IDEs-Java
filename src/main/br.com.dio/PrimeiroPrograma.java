785a4fc7f12c158a8aa82b80e057956578063d75
Pedro Henrique <pedrohenriqueboy000@gmail.com>
2022-05-05 17:12:07 -0300

    Primeiro commit pelo intellij

--- src/main/br.com.dio/PrimeiroPrograma.java
+++ src/main/br.com.dio/PrimeiroPrograma.java
@@ -4,7 +4,7 @@ import main.br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-        Gato gato = new Gato("zezinho", "preto", 10);
+        Gato gato = new Gato("Garfield", "preto", 10);
 
         System.out.println(gato);
 
